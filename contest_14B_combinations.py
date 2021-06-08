581127fea0619518e8cd809687c949fb7a3849d7
Vladimir Sumenko <3760097@gmail.com>
2021-06-08 21:12:57 +0300

    start 14B combinations

---
+++ contest_14B_combinations.py
@@ -0,0 +1,20 @@
+# Спринт 14
+# B. Комбинации
+
+kbd = {
+    '2': 'abc', '3': 'def',
+    '4': 'ghi', '5': 'jkl', '6': 'mno',
+    '7': 'pqrs','8': 'tuv', '9': 'wxyz'
+}
+
+def generator(numbers, txt):
+    pass
+
+
+def main():
+    generator('2 3'.split(), '')
+
+
+if __name__ == '__main__':
+    main()
+    # 23 ad ae af bd be bf cd ce cf
