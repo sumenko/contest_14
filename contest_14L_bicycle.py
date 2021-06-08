9e16bb21596672a7d8c159f68d255d1de80e753f
Vladimir Sumenko <3760097@gmail.com>
2021-06-08 20:11:21 +0300

    14L bicycle - done

--- contest_14L_bicycle.py
+++ contest_14L_bicycle.py
@@ -1,13 +1,16 @@
-# Два велосипеда
+# Спринт 14
+# L. Два велосипеда
+# в отличии от бинарного поиска убрано строгое равенство num == arr[mid]
 
 def bin_search(arr, num, left, right):
     """ Бинарный поиск num, arr - неубывающий список """
     if right <= left:
-        return -1
+        if left == len(arr):
+            return -1
+        return left + 1
+
     mid = (right + left) // 2
-    if arr[mid] == num:
-        return mid
-    elif num < arr[mid]:
+    if num <= arr[mid]:
         # go left
         return bin_search(arr, num, left, mid)
     else:
@@ -18,18 +21,28 @@ def bin_search(arr, num, left, right):
 def which_day(n, money, cost):
     arr = [int(num) for num in money.split()]
     a = bin_search(arr[:n], cost, 0, n)
-    # print('look for nearest greater than _{}_ in {}. result: {}'.format(cost, money, a))
-    return a
+    b = bin_search(arr[:n], cost * 2, 0, n)
+    return a, b
+
+
+def main():
+    n = int(input())
+    money = input()
+    cost = int(input())
+    print(*which_day(n, money, cost))
 
 
 if __name__ == '__main__':
-    assert which_day(6, '1 2 4 4 6 8', 1) == 0
-    assert which_day(6, '1 2 4 4 6 8', 2) == 1
-    assert which_day(6, '1 2 4 4 6 8', 4) == 3
-    assert which_day(6, '1 2 4 4 6 8', 6) == 4
-    assert which_day(6, '1 2 4 4 6 8', 8) == 5
-    assert which_day(6, '1 2 4 4 6 8', 3) == -1
-    # print(which_day(6, '1 2 4 4 6 8', 3))
-    # assert which_day(6, '1 2 4 4 6 8', 3) == (3, 5)
-    # assert which_day(6, '1 2 4 4 4 4', 3) == (3, -1)
-    # assert which_day(6, '1 2 4 4 4 4', 10) == (-1, -1)
+    main()
+    assert which_day(6, '1 2 4 4 6 8', 1) == (1, 2)
+    assert which_day(6, '1 2 4 4 6 8', 2) == (2, 3)
+    assert which_day(6, '1 2 4 4 6 8', 4) == (3, 6)
+    assert which_day(6, '1 2 4 4 6 8', 6) == (5, -1)
+    assert which_day(6, '1 2 4 4 6 8', 8) == (6, -1)
+    assert which_day(6, '4 4 4 4 6 8', 4) == (1, 6)
+    assert which_day(6, '4 4 4 4 6 6', 4) == (1, -1)
+
+
+    assert which_day(6, '1 2 4 4 6 8', 3) == (3, 5)
+    assert which_day(6, '1 2 4 4 4 4', 3) == (3, -1)
+    assert which_day(6, '1 2 4 4 4 4', 10) == (-1, -1)
