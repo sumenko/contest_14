4126e098d46e6b4947168e4b219a1f60b4fc6329
Vladimir Sumenko <3760097@gmail.com>
2021-06-19 01:07:49 +0300

    QuickSort class with smart init added

--- final_14b.py
+++ final_14b.py
@@ -1,62 +1,66 @@
-from random import expovariate, randint, randrange
+from random import randint
 
 
 def gte(a, b):
     login_a, p_a, f_a = a
     login_b, p_b, f_b = b
-    print(login_a, login_b)
-    if (p_a > p_b or  (p_a == p_b and f_a < f_b) or
-        (p_a == p_b and f_a == f_b and login_a < login_b)):
+
+    if (p_a > p_b or (p_a == p_b and f_a < f_b) or
+       (p_a == p_b and f_a == f_b and login_a < login_b)):
         return True
     return False
 
 
 def lte(a, b):
-    return gte(b, a)
+    return not gte(a, b)
 
 
-def print_arr(arr, left, right):
-    print(''.join([f'{i:<4}' for i in arr]))
+class QuickSort():
+    def __init__(self, gte_func=None, lte_func=None, reverse=False):
+        if not gte_func:
+            self.gte = lambda a, b: a > b
+        else:
+            self.gte = gte
 
-    s = ['.'] * len(arr)
-    s[left] = 'l'
-    if left != right: 
-        s[right] = 'r'
-    else:
-        s[left] = '*'
-    print(''.join([f'{i:<4}' for i in s]), '\n')
+        if not lte_func:
+            self.lte = lambda a, b: a < b
+        else:
+            self.lte = lte
 
+        if reverse:
+            self.lte, self.gte = self.gte, self.lte
 
-def quick_sort(arr, left=0, right=None):
-    if not right:
-        right = len(arr) - 1
-    pivot = arr[randint(left, right)]
-    l = left
-    r = right
+    def sort(self, arr, left=0, right=None):
+        if not right:
+            right = len(arr) - 1
+        pivot = arr[randint(left, right)]
+        l_ptr = left
+        r_ptr = right
 
-    while l <= r:
-        while arr[l] < pivot and l < right:
-            l += 1
-        while pivot <arr[r] and r > left:
-            r -= 1
+        while l_ptr <= r_ptr:
+            while self.lte(arr[l_ptr], pivot) and l_ptr < right:
+                l_ptr += 1
+            while self.lte(pivot, arr[r_ptr]) and r_ptr > left:
+                r_ptr -= 1
 
-        if l <= r:
-            arr[l], arr[r] = arr[r], arr[l]
-            l += 1
-            r -= 1
+            if l_ptr <= r_ptr:
+                arr[l_ptr], arr[r_ptr] = arr[r_ptr], arr[l_ptr]
+                l_ptr += 1
+                r_ptr -= 1
 
-    if r > left:
-        quick_sort(arr, left, r)
-    if l < right:
-        quick_sort(arr, l, right)
+        if r_ptr > left:
+            self.sort(arr, left, r_ptr)
+        if l_ptr < right:
+            self.sort(arr, l_ptr, right)
 
 
 def main():
     pass
 
+
 if __name__ == '__main__':
     main()
-        
+
 # """5
 # alla 4 100
 # gena 6 1000
--- final_14b_test.py
+++ final_14b_test.py
@@ -2,7 +2,7 @@ import unittest
 from random import randint
 from unittest import TestCase
 
-from final_14b import gte, lte, quick_sort
+from final_14b import QuickSort, gte, lte
 
 
 class TestQuickSort(TestCase):
@@ -17,16 +17,17 @@ class TestQuickSort(TestCase):
             [1, 2, 3, 4, 0],
             [randint(0, 10) for _ in range(10)],
         )
-        with self.subTest():
+        q = QuickSort()
+        with self.subTest(gte=gte, lte=lte):
             for n, test in enumerate(tests):
                 print('Test #', n)
-                quick_sort(test, 0, len(test) - 1)
+                q.sort(test)
                 try:
                     srt = sorted(test)
                     self.assertEqual(test, srt)
                 except AssertionError:
                     print(*test, '!=', *srt)
-    
+
     def test_gte(self):
         alla = ('alla', 4, 100)
         gena = ('gena', 6, 1000)
@@ -38,7 +39,7 @@ class TestQuickSort(TestCase):
         self.assertTrue(gte(timofey, alla))
         self.assertTrue(gte(gosha, rita))
         self.assertTrue(gte(timofey, gosha))
-    
+
     def test_lte(self):
         alla = ('alla', 4, 100)
         gena = ('gena', 6, 1000)
@@ -50,6 +51,20 @@ class TestQuickSort(TestCase):
         self.assertFalse(lte(timofey, alla))
         self.assertFalse(lte(gosha, rita))
         self.assertFalse(lte(timofey, gosha))
+
+    def test_sort_with_lte(self):
+        test = [
+            ('alla', 4, 100),
+            ('gena', 6, 1000),
+            ('gosha', 2, 90),
+            ('rita', 2, 90),
+            ('timofey', 4, 80),
+        ]
+        q = QuickSort(gte, lte, reverse=True)
+        q.sort(test)
+        answer = [t[0] for t in test]
+        self.assertEqual(answer, ['gena', 'timofey', 'alla', 'gosha', 'rita'])
+
 
 if __name__ == '__main__':
     unittest.main()
